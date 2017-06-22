adf9731c6c35c9f9c521fb762274d9cf12e110e8
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-22 11:45:52 +0000

    Exercise 8:5 Add more informartion about the students

--- directory.rb
+++ directory.rb
@@ -3,23 +3,28 @@ def input_students
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name 
+  # get the first name
+  puts "Enter a name"
   name = gets.chomp 
+  
   # while the name is not empty, repeat this code 
   while !name.empty? do 
+    puts "Enter student's country"
+    country = gets.chomp 
+  
+    puts "Enter the student's age"
+    age = gets.chomp 
+    
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, country: country, age: age, cohort: :november}
     puts "Now we have #{students.count}"
+    
     # get another name from the user 
+    puts "Enter a name"
     name = gets.chomp 
   end 
   # return the array of students 
   students 
-end 
-
-def print_header 
-  puts "The students of Villians Academy"
-  puts "-------------"
 end 
 
 def sort_students(students)
@@ -28,16 +33,20 @@ def sort_students(students)
     first_letter = student[:name][0].downcase
     if first_letter == "c" && student[:name].length < 12
       sorted_students << student
-    end 
-  end 
+    end
+  end
   return sorted_students
-end  
+end
 
+def print_header 
+  puts "The students of Villians Academy"
+  puts "-------------"
+end 
 
 def print(students)
  i = 0 
   until i >= students.length
-   puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+   puts "#{i + 1}. #{students[i][:name]} #{students[i][:country]} #{students[i][:age]} (#{students[i][:cohort]} cohort)"
   i += 1 
   end  
 end 
@@ -47,7 +56,7 @@ def print_footer(students)
 end 
 
 students = input_students
-students = sort_students(students)
+#students = sort_students(students)  if you want print out the sorted list.
 #nothing happens until we call the methods
 
 print_header
