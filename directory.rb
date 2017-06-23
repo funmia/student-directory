b48ce5690967473ec6de344c301e999eeb07a3e8
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-23 10:41:02 +0000

    Adds an interactive menu & updates the program to call the interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,5 +1,32 @@
 COHORT_LIST =[:january,:february,:march,:april,:may,:june,:july,:august,:september,:october,:november,:december]
 
+def interactive_menu 
+  students = []
+  loop do 
+    # print the menu and ask the user what to do 
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" 
+    # read the input and save it in a vaiable 
+    selection = gets.chomp 
+    #do what the user has asked 
+    case selection 
+      when "1"
+       #input the students 
+       students = input_students()
+      when "2"
+        # show the students 
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        break #exit #this will cause the program to terminate 
+      else 
+        puts "I don't know what you meant, try again"
+      end 
+  end
+end  
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -65,11 +92,12 @@ end
  
 def print_footer(students)
   center_align("Overall, we have #{students.count} great students")
+  puts 
 end 
 
-students = input_students
+interactive_menu()
 
-print_header()
-print(students)
-print_footer(students)
+
+
+
 
