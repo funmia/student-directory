b6e28c3cfd91ef819a60b379cf21543991c1561f
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-23 22:24:24 +0000

    Exercise 13 Adds functionality to allow taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -2,33 +2,21 @@ COHORT_LIST =[:january,:february,:march,:april,:may,:june,:july,:august,:septemb
 
 @students = [] #an empty array accessible to all methods
 
-def save_students
-  #open the file for writing 
-  file = File.open("students.csv", "w")
-  # iterate over the array of students 
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end 
-  file.close 
-end 
-
-def load_students 
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end 
-  file.close 
-end 
+def print_menu
+ # print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"       # 9 because we'll be adding more items
+end
 
 def interactive_menu
   loop do
     # print the menu of options
     print_menu
     # read input and save to a variable
-    selection = gets.chomp
+    selection = STDIN.gets.chomp
     # do what the user has asked
     process(selection)
   end
@@ -54,30 +42,34 @@ def process(selection)
     end
 end
 
-def print_menu
- # print the menu and ask the user what to do
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"       # 9 because we'll be adding more items
-end
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
 
-def show_students
-  print_header()
-  print_students_list()
-  print_footer()
-end
+  puts "Enter a name"
+  name = STDIN.gets.delete("\n")
 
-# Aligns the text to the center
-def center_align(text)
-  width = 200
-  puts text.center(width)
-end
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Enter the student's cohort"
+    cohort = STDIN.gets.delete("\n").to_sym
 
-def print_header
-  center_align("The students of Villians Academy")
-  center_align( "-------------")
+    if cohort.empty?
+      cohort = :not_decided
+    end
+
+    # add the student hash to the array
+    @students << {name: name, cohort: cohort}
+
+    if @students.size > 1
+     puts "Now we have #{@students.count} students"
+    else
+      puts "Now we have #{@students.count} student"
+    end
+    # get another name from the user
+    puts "Enter a name"
+    name = STDIN.gets.delete("\n")
+  end
 end
 
 def show_students
@@ -113,35 +105,39 @@ def print_footer
   puts
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+def save_students
+  #open the file for writing 
+  file = File.open("students.csv", "w")
+  # iterate over the array of students 
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end 
+  file.close 
+end 
 
-  puts "Enter a name"
-  name = gets.delete("\n")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end 
+  file.close 
+end 
 
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    puts "Enter the student's cohort"
-    cohort = gets.delete("\n").to_sym
+def try_load_students
+  filename = ARGV.first            # first argument from the command line  
+  return if filename.nil?         # get out of the method if it isn't given
+  if File.exists?(filename)      #if it exists 
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else  # if it doesn't exist 
+    puts "Sorry, #{filename} doesn't exist."
+    exit  # quit the program 
+  end 
+end 
 
-    if cohort.empty?
-      cohort= :not_decided
-    end
-
-    # add the student hash to the array
-    @students << {name: name, cohort: cohort}
-
-    if @students.size > 1
-     puts "Now we have #{@students.count} students"
-    else
-      puts "Now we have #{@students.count} student"
-    end
-    # get another name from the user
-    puts "Enter a name"
-    name = gets.delete("\n")
-  end
-end
-
+try_load_students()
 interactive_menu()
 
