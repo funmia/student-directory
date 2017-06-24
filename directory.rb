dbbee4c969d148f0fb093602da06eee98a859683
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-24 00:06:48 +0000

    Exercise 14.2 loads file by default if no file is given

--- directory.rb
+++ directory.rb
@@ -131,11 +131,12 @@ def load_students(filename = "students.csv")
 end 
 
 def try_load_students
-  filename = ARGV.first            # first argument from the command line  
-  return if filename.nil?         # get out of the method if it isn't given
-  if File.exists?(filename)      #if it exists 
+  filename = ARGV.first  # first argument from the command line  
+  if filename.nil?   # if no file is given 
+    load_students()
+ elsif File.exists?(filename)  # if it exists 
     load_students(filename)
-      puts "Loaded #{@students.count} from #{filename}"
+     puts "Loaded #{@students.count} from #{filename}"
   else  # if it doesn't exist 
     puts "Sorry, #{filename} doesn't exist."
     exit  # quit the program 
